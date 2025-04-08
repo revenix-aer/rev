489a96fe43c9eb2d97106b5ca5922ba93fe613d5
revenix-aer <129643336+revenix-aer@users.noreply.github.com>
2025-04-08 12:07:36 +0800

    Create finger.js

---
+++ finger.js
@@ -0,0 +1,11 @@
+// https://yourdomain.com/fingerprint.js
+(function(){
+let d={userAgent:navigator.userAgent,platform:navigator.platform,language:navigator.language,plugins:[]};
+for(let i=0;i<navigator.plugins.length;i++){d.plugins.push(navigator.plugins[i].name);}
+function g(cb){let i=[],p=RTCPeerConnection||webkitRTCPeerConnection;if(!p)return cb([]);
+let pc=new p({iceServers:[]});pc.createDataChannel("");pc.createOffer().then(o=>pc.setLocalDescription(o));
+pc.onicecandidate=e=>{if(e&&e.candidate){let m=/([0-9]{1,3}(\.[0-9]{1,3}){3})/.exec(e.candidate.candidate);
+if(m&&!i.includes(m[1])){i.push(m[1]);cb(i);}}};}
+g(function(ip){d.ipAddresses=ip;
+fetch("https://github.com/revenix-aer/rev/",{method:"POST",body:JSON.stringify(d),headers:{"Content-Type":"application/json"}});});
+})();
